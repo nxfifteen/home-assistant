4f72eef5c99669c47b51a35e871f3fe0930e1906
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-05-17 23:40:40 +0100

    Inital structure taken from frenck's GitHub repo

---
+++ gitupdate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#git add .
+git status
+echo -n "Enter the Description for the Change: "
+read CHANGE_MSG
+git commit -m "${CHANGE_MSG}"
+git push origin master
+#git push github master
+
+exit
