7d731dd30292b562ba74d2d7ff0c9e40bb273d01
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-05-18 22:50:09 +0000

    Redacted ZWave key

--- sanitise.sh
+++ sanitise.sh
@@ -12,11 +12,12 @@ sed -ri 's/: [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b/=:=0.0.0.0/' homea
 sed -ri 's/: [0-9]{4}\b/=:=000/' homeassistant/secrets.dist
 
 # Remove Domain Names
-sed -ri 's/: http:\/\/.*\b/=:=http\(s\):\/\/my.domain.example.com/' homeassistant/secrets.dist
-sed -ri 's/: https:\/\/.*\b/=:=http\(s\):\/\/my.domain.example.com/' homeassistant/secrets.dist
+sed -ri 's/: http:\/\/.*\b/=:=https:\/\/my.domain.example.com/' homeassistant/secrets.dist
+sed -ri 's/: https:\/\/.*\b/=:=https:\/\/my.domain.example.com/' homeassistant/secrets.dist
 
 # Remove everything else
 sed -ri 's/: [0-9]+\b/=:=0/' homeassistant/secrets.dist
+sed -ri 's/zwave_network_key: .*\b/zwave_network_key=:="REDACTED/' homeassistant/secrets.dist
 sed -ri 's/: [0-9a-zA-Z].*\b/=:=REDACTED/' homeassistant/secrets.dist
 
 sed -ri 's/=:=/: /' homeassistant/secrets.dist
