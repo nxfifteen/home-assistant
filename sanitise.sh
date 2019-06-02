d65ce3f3909fed1c2fc8661d8f584a8f35c3a818
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-06-02 14:04:28 +0100

    Check in Sunday, June 02 @14:04:28

--- sanitise.sh
+++ sanitise.sh
@@ -9,7 +9,8 @@ sed -ri 's/: \-[0-9]{1,3}\.[0-9]{13,16}\b/=:=0.0000000000000/' homeassistant/sec
 sed -ri 's/: [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b/=:=0.0.0.0/' homeassistant/secrets.dist
 
 # Remove port numbers
-sed -ri 's/: [0-9]{4}\b/=:=1/' homeassistant/secrets.dist
+sed -ri 's/:[0-9]{4}\b/:1234/' homeassistant/secrets.dist
+sed -ri 's/: [0-9]{4}\b/=:=1234/' homeassistant/secrets.dist
 
 # Remove Domain Names
 sed -ri 's/: http:\/\/.*\b/=:=https:\/\/my.domain.example.com/' homeassistant/secrets.dist
@@ -20,13 +21,15 @@ sed -ri 's/: [0-9]+\b/=:=0/' homeassistant/secrets.dist
 sed -ri 's/zwave_network_key: .*\b/zwave_network_key=:="REDACTED/' homeassistant/secrets.dist
 sed -ri 's/: [0-9a-zA-Z].*\b/=:=REDACTED/' homeassistant/secrets.dist
 
+# Remove Curl command string
+sed -ri "s/curl.*/\/path\/to\/curl\/command/" homeassistant/secrets.dist
+
+# Hog-pog clean up - Find the mess and fix it
+sed -ri "s/com\//com/" homeassistant/secrets.dist
 sed -ri 's/"//' homeassistant/secrets.dist
 sed -ri "s/'//" homeassistant/secrets.dist
 sed -ri "s/'//" homeassistant/secrets.dist
 
-sed -ri "s/REDACTED/REDACTED'/" homeassistant/secrets.dist
-sed -ri "s/=:=REDACTED/=:='REDACTED/" homeassistant/secrets.dist
-sed -ri "s/curl/'curl/" homeassistant/secrets.dist
-
+# Make everything formatted the YAML way
 sed -ri 's/=:=/: /' homeassistant/secrets.dist
 
