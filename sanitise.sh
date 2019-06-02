e3652a7fe6f982b76059b30c94586182e4e7a08c
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-06-02 13:11:50 +0100

    Check in Sunday, June 02 @13:11:50

--- sanitise.sh
+++ sanitise.sh
@@ -9,7 +9,7 @@ sed -ri 's/: \-[0-9]{1,3}\.[0-9]{13,16}\b/=:=0.0000000000000/' homeassistant/sec
 sed -ri 's/: [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b/=:=0.0.0.0/' homeassistant/secrets.dist
 
 # Remove port numbers
-sed -ri 's/: [0-9]{4}\b/=:=000/' homeassistant/secrets.dist
+sed -ri 's/: [0-9]{4}\b/=:=1/' homeassistant/secrets.dist
 
 # Remove Domain Names
 sed -ri 's/: http:\/\/.*\b/=:=https:\/\/my.domain.example.com/' homeassistant/secrets.dist
@@ -19,6 +19,10 @@ sed -ri 's/: https:\/\/.*\b/=:=https:\/\/my.domain.example.com/' homeassistant/s
 sed -ri 's/: [0-9]+\b/=:=0/' homeassistant/secrets.dist
 sed -ri 's/zwave_network_key: .*\b/zwave_network_key=:="REDACTED/' homeassistant/secrets.dist
 sed -ri 's/: [0-9a-zA-Z].*\b/=:=REDACTED/' homeassistant/secrets.dist
+
+sed -ri 's/"//' homeassistant/secrets.dist
+sed -ri "s/'//" homeassistant/secrets.dist
+sed -ri "s/'//" homeassistant/secrets.dist
 
 sed -ri 's/=:=/: /' homeassistant/secrets.dist
 
